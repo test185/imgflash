30885dc87fe458b92e230b9c209b5ccf6aaefde5
GuangYu-yu <77334277+GuangYu-yu@users.noreply.github.com>
2026-06-30 01:14:59 +0800

    Update create-template.sh

--- create-template.sh
+++ create-template.sh
@@ -371,6 +371,7 @@ if [[ "${HAS_BIOS}" -eq 1 ]]; then
         -isohybrid-mbr "${ISOHDPFX_PATH}" \
         -eltorito-boot boot/syslinux/isolinux.bin \
             -no-emul-boot \
+            -boot-info-table \
             --eltorito-catalog boot/syslinux/boot.cat \
         -eltorito-alt-boot \
             -e boot/grub/efi.img \
